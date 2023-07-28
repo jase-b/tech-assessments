517dda7ee60cf94eeeaccfdb507a415a4700145d
Jase Balderrama <jasebalderrama@icloud.com>
2023-07-28 15:51:14 -0700

    Change metaUrlPath dir name

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -3,6 +3,7 @@ export default {
     src: "/",
   },
   buildOptions: {
+    metaUrlPath: "snowpack", // The '_' in the default name of '_snowpack' causes error when loading an extension directory.
     watch: true,
   },
 };
