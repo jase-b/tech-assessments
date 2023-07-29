db73e36d66160734b2f8a0e9c7a85a22cc59eb68
Jase Balderrama <jasebalderrama@icloud.com>
2023-07-28 17:58:05 -0700

    Implement TypeScript

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,15 +1,15 @@
 export default {
-  mount: {
-    src: "/",
-  },
   buildOptions: {
     metaUrlPath: "snowpack", // '_' in default name '_snowpack' causes error when loading an extension directory.
     out: "dist",
     watch: true,
   },
-  optimize: {
-    bundle: true,
-    minify: true,
-    target: "es2020",
+  mount: {
+    src: "/",
   },
+  plugins: [
+    "@snowpack/plugin-react-refresh",
+    "@snowpack/plugin-dotenv",
+    ["@snowpack/plugin-typescript"],
+  ],
 };
