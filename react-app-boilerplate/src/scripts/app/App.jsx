b321244804559e69a3e1e924d460b2f2b3e9d2e4
Jase Balderrama <jasebalderrama@icloud.com>
2023-09-17 17:47:27 -0700

    Add page nav buttons

--- react-app-boilerplate/src/scripts/app/App.jsx
+++ react-app-boilerplate/src/scripts/app/App.jsx
@@ -3,41 +3,58 @@ import PropTypes from 'prop-types';
 import './App.css';
 
 const App = () => {
-  const [tiffFile, setTiffFile] = useState(null);
-  const [canvas, setCanvas] = useState(null);
+  const [tiff, setTiff] = useState(null);
+  const [currentPage, setCurrentPage] = useState((tiff?.currentDirectory() || 0) + 1);
+  const [totalPages, setTotalPages] = useState(0);
 
   const handleFileInputChange = (e) => {
     const file = e.target.files[0];
-    setTiffFile(file);
+
+    if (file) {
+      file.arrayBuffer().then(buffer => {
+        const tiff = new Tiff({ buffer });
+
+        setTiff(tiff);
+        console.log(tiff?.currentDirectory())
+        totalPages === 0 && setTotalPages(tiff.countDirectory());
+      })
+    }
+  };
+
+  const addTiffCanvastoPage = () => {
+    const canvas = document.querySelector('.canvas');
+
+    canvas.textContent = '';
+    canvas.append(tiff.toCanvas());
+  };
+
+  const setPageNumber = (pageNum) => {
+    tiff.setDirectory(pageNum - 1);
+    setCurrentPage(pageNum);
+    addTiffCanvastoPage();
   };
 
   useEffect(() => {
-    if (tiffFile) {
-      tiffFile.arrayBuffer().then(buffer => {
-        const tiff = new Tiff({ buffer });
-
-        // tiff.setDirectory(5);
-
-        console.log('tiff', tiff);
-        console.log('countDirectory()', tiff.countDirectory());
-        console.log('currentDirectory()', tiff.currentDirectory());
-
-        document.querySelector('.canvas').append(tiff.toCanvas());
-      })
+    if (tiff) {
+      addTiffCanvastoPage();
     }
-  }, [tiffFile]);
+  }, [tiff]);
 
   return (
     <>
       <header></header>
       <main>
-        {!tiffFile ? (
+        {!tiff ? (
           <label className='drop-zone' htmlFor='file-input'>
             <p className='drop-zone__text'>Click to Upload</p>
             <input aria-label="file-input" id='file-input' onChange={handleFileInputChange} type='file' />
           </label>
         ) : <div className='canvas'></div>}
-        
+        <div className='buttons-wrapper'>
+          <i className={["fa-solid", "fa-circle-arrow-left", currentPage === 1 ? 'disabled' : ''].join(" ")} onClick={() => currentPage > 1 && setPageNumber(currentPage - 1)}></i>
+          <p className='page-tracker'>Page {currentPage} / {totalPages}</p>
+          <i className={['fa-solid ', 'fa-circle-arrow-right', currentPage === totalPages ? 'disabled' : ''].join(" ")} onClick={() => currentPage < totalPages && setPageNumber(currentPage + 1)}></i>
+        </div>
       </main>
     </>
   );
